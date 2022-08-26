90951d33327ab3213812b539602abd2b7fdc8691
LoganStyles <emmaist23@gmail.com>
2022-08-26 14:45:21 +0100

    feat(add data using entitystate): update program.cs to reflect entitystate

--- Program.cs
+++ Program.cs
@@ -1,17 +1,19 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
 using SaveData;
 
-class Program{
+class Program
+{
 
-    public static void Main(string[] args){
+    public static void Main(string[] args)
+    {
 
         var context = new ArtistsContext();
 
         //create new instance of employee
-        var employee = new Employee{ FirstName="Susan", LastName="Fredricks", Age=28};
+        var employee = new Employee { FirstName = "Susan", LastName = "Fredricks", Age = 28 };
 
         //begin tracking the Employee entity
-        context.Employees.Add(employee);
+        context.Entry(employee).State = EntityState.Added;
         //save all changes made in this context to the database
         context.SaveChanges();
     }
