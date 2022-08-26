bbe9f39110e7bc7e10701fc4084be7f2db7ec89b
LoganStyles <emmaist23@gmail.com>
2022-08-26 14:01:21 +0100

    feat(save data - add data): update artists.sql with insert statements

--- Artists.sql
+++ Artists.sql
@@ -25,5 +25,13 @@ CREATE INDEX "IX_Albums_EmployeeId" ON "Albums" ("EmployeeId");
 INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
 VALUES ('20220826125411_InitialMigration', '6.0.2');
 
+INSERT INTO Employees values(1,'Max','Bello',25);
+INSERT INTO Employees values(2,'Francis','Ojukwu',34);
+INSERT INTO Employees values(3,'Martha','Bertha',19);
+
+INSERT INTO Albums values(1,'Blue Fire',2000,2);
+INSERT INTO Albums values(2,'Raging Heart',3850,3);
+INSERT INTO Albums values(3,'Fixated on you',4000,1);
+
 COMMIT;
 
