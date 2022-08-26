ee71ee52271e8d26bc8b52669409bcf73cc7eb95
LoganStyles <emmaist23@gmail.com>
2022-08-26 14:11:36 +0100

    feat(save data - add related entities): updated album, artists.sql, artists.sh to use fully defined relationship

--- Artists.sh
+++ Artists.sh
@@ -18,14 +18,22 @@ CREATE TABLE "Albums" (
     "Id" INTEGER NOT NULL CONSTRAINT "PK_Albums" PRIMARY KEY AUTOINCREMENT,
     "Title" TEXT NULL,
     "Price" REAL NOT NULL,
-    "EmployeeId" INTEGER NULL,
-    CONSTRAINT "FK_Albums_Employees_EmployeeId" FOREIGN KEY ("EmployeeId") REFERENCES "Employees" ("Id")
+    "EmployeeId" INTEGER NOT NULL,
+    CONSTRAINT "FK_Albums_Employees_EmployeeId" FOREIGN KEY ("EmployeeId") REFERENCES "Employees" ("Id") ON DELETE CASCADE
 );
 
 CREATE INDEX "IX_Albums_EmployeeId" ON "Albums" ("EmployeeId");
 
 INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
-VALUES ('20220826052038_EmployeeAlbumSetup', '6.0.2');
+VALUES ('20220826130935_InitialMigration', '6.0.2');
+
+INSERT INTO Employees values(1,'Max','Bello',25);
+INSERT INTO Employees values(2,'Francis','Ojukwu',34);
+INSERT INTO Employees values(3,'Martha','Bertha',19);
+
+INSERT INTO Albums values(1,'Blue Fire',2000,2);
+INSERT INTO Albums values(2,'Raging Heart',3850,3);
+INSERT INTO Albums values(3,'Fixated on you',4000,1);
 
 COMMIT;
 EOF
