10c3c03a0b60222524cfdf9dbc7db43a0de068da
LoganStyles <emmaist23@gmail.com>
2022-08-26 06:17:48 +0100

    feat(save data): updated program.cs with code for adding an employee instance

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using SaveData;
+
+class Program{
+
+    public static void Main(string[] args){
+
+        var context = new ArtistsContext();
+
+        //create new instance of employee
+        var employee = new Employee{ FirstName="Susan", LastName="Fredricks", Age=28};
+
+        //begin tracking the Employee entity
+        context.Employees.Add(employee);
+        //save all changes made in this context to the database
+        context.SaveChanges();
+    }
+}
