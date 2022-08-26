09ee449ac9037f53cda5e197da76eafaa042c334
LoganStyles <emmaist23@gmail.com>
2022-08-26 11:26:45 +0100

    feat(save data): modified program.cs to reflect saving related entities

--- Program.cs
+++ Program.cs
@@ -1,16 +1,31 @@
 ﻿
 using SaveData;
 
-class Program{
+class Program
+{
 
-    public static void Main(string[] args){
+    public static void Main(string[] args)
+    {
 
         var context = new ArtistsContext();
 
         //create new instance of employee
-        var employee = new Employee{ FirstName="Susan", LastName="Fredricks", Age=28};
+        var employee = new Employee
+        {
+            FirstName = "Steven",
+            LastName = "Brandy",
+            Age = 30,
+            Albums = new List<Album>{
 
-        //begin tracking the Employee entity
+                new Album { Title="Lovely skys", Price=2500},
+                new Album { Title="Joy of the sun", Price=4000},
+
+            }
+        };
+
+        /*begin tracking the Employee entity and 
+        any other reachable entities that are not being tracked
+        */
         context.Employees.Add(employee);
         //save all changes made in this context to the database
         context.SaveChanges();
