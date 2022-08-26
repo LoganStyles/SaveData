1cdb6c13b96d55a0bce9257392b434e086bb381b
LoganStyles <emmaist23@gmail.com>
2022-08-26 06:25:18 +0100

    feat(save data): added artists.sql and artists.sh to reflect structure of database

---
+++ Artists.sql
@@ -0,0 +1,29 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" TEXT NOT NULL CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY,
+    "ProductVersion" TEXT NOT NULL
+);
+
+BEGIN TRANSACTION;
+
+CREATE TABLE "Employees" (
+    "Id" INTEGER NOT NULL CONSTRAINT "PK_Employees" PRIMARY KEY AUTOINCREMENT,
+    "FirstName" TEXT NULL,
+    "LastName" TEXT NULL,
+    "Age" INTEGER NOT NULL
+);
+
+CREATE TABLE "Albums" (
+    "Id" INTEGER NOT NULL CONSTRAINT "PK_Albums" PRIMARY KEY AUTOINCREMENT,
+    "Title" TEXT NULL,
+    "Price" REAL NOT NULL,
+    "EmployeeId" INTEGER NULL,
+    CONSTRAINT "FK_Albums_Employees_EmployeeId" FOREIGN KEY ("EmployeeId") REFERENCES "Employees" ("Id")
+);
+
+CREATE INDEX "IX_Albums_EmployeeId" ON "Albums" ("EmployeeId");
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20220826052038_EmployeeAlbumSetup', '6.0.2');
+
+COMMIT;
+
