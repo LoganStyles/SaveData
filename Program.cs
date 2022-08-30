b8ef09692bdaf67a05610f9f890fd60a63524d13
LoganStyles <emmaist23@gmail.com>
2022-08-30 07:43:47 +0100

    feat( save data - add data): add equivalent code for adding data using DbContext.Add method

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ class Program
 
         //begin tracking the Employee entity
         context.Employees.Add(employee);
+        // context.Add<Employee>(employee);
         //save all changes made in this context to the database
         context.SaveChanges();
     }
