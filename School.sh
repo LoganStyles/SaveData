7a56a0ba3177b574b2c24549d1380fe16d564db2
LoganStyles <emmaist23@gmail.com>
2022-11-02 11:47:13 +0100

    feat( save data challenge): update the shell script with StudentCourse and Course schema

--- School.sh
+++ School.sh
@@ -2,6 +2,12 @@
 sqlite3 output/School.db <<EOF
 BEGIN TRANSACTION;
 
+CREATE TABLE "Courses" (
+    "Id" INTEGER NOT NULL CONSTRAINT "PK_Courses" PRIMARY KEY AUTOINCREMENT,
+    "Title" TEXT NULL,
+    "DepartmentCode" INTEGER NOT NULL
+);
+
 CREATE TABLE "Departments" (
     "Id" INTEGER NOT NULL CONSTRAINT "PK_Departments" PRIMARY KEY AUTOINCREMENT,
     "Title" TEXT NULL,
@@ -23,7 +29,17 @@ CREATE TABLE "Instructors" (
     CONSTRAINT "FK_Instructors_Departments_DepartmentCode" FOREIGN KEY ("DepartmentCode") REFERENCES "Departments" ("Id") ON DELETE CASCADE
 );
 
+CREATE TABLE "StudentCourses" (
+    "StudentId" INTEGER NOT NULL,
+    "CourseId" INTEGER NOT NULL,
+    CONSTRAINT "PK_StudentCourses" PRIMARY KEY ("StudentId", "CourseId"),
+    CONSTRAINT "FK_StudentCourses_Courses_CourseId" FOREIGN KEY ("CourseId") REFERENCES "Courses" ("Id") ON DELETE CASCADE,
+    CONSTRAINT "FK_StudentCourses_Students_StudentId" FOREIGN KEY ("StudentId") REFERENCES "Students" ("Id") ON DELETE CASCADE
+);
+
 CREATE INDEX "IX_Instructors_DepartmentCode" ON "Instructors" ("DepartmentCode");
+
+CREATE INDEX "IX_StudentCourses_CourseId" ON "StudentCourses" ("CourseId");
 
 COMMIT;
 EOF
