9e50018f8b74b4570d73a03805b6ccc738606016
LoganStyles <emmaist23@gmail.com>
2022-12-03 06:33:59 +0100

    feat(add data): updated program.cs to display all employees

--- Program.cs
+++ Program.cs
@@ -9,12 +9,19 @@ class Program
         using var context = new ArtistsContext();
 
         //create new instance of employee
-        var employee = new Employee { FirstName = "Susan", LastName = "Fredricks", Age = 28 };
+        var employee = new Employee { FirstName = "Susan", LastName = "Fred", Age = 28 };
 
         //begin tracking the Employee entity
         context.Employees.Add(employee);
         // context.Add<Employee>(employee);
         //save all changes made in this context to the database
         context.SaveChanges();
+
+        var allEmployees = context.Employees.ToList();
+        Console.WriteLine("Id \tFirstName \tLastName \tAge");
+        foreach (var emp in allEmployees)
+        {
+            Console.WriteLine($"{emp.Id}\t{emp.FirstName}\t\t{emp.LastName}\t\t{emp.Age}");
+        }
     }
 }
